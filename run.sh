7e661a4815f8093aed19469b6733abcb1aeab217
mullinix <james.mullinix@gmail.com>
2019-06-05 15:44:08 -0700

    remove hard-coded start directory in favor of argin

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+SOURCE_DIR=$1
 XAUTH=$HOME/.Xauthority
 touch $XAUTH
 docker run -it --rm \
   -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v $XAUTHORITY:/home/developer/.Xauthority \
-  -v /home/james/GDriveSDSU-w/Course\ And\ Student\ Data/:/data \
+  -v $SOURCE_DIR/:/data \
   -w /data mullinix/nvidia-opengl-anaconda3-spyder:from-singularity-recipe
 
