94cbfd025ec9f844342580c4692c4f5da42413b1
mullinix <james.mullinix@gmail.com>
2019-06-05 16:17:59 -0700

    set default directory to pwd if none is entered

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-SOURCE_DIR=$1
+#SOURCE_DIR=$1
+if [ "$1" = "" ]; then
+	SOURCE_DIR=$(pwd)
+else
+	SOURCE_DIR=$1
+fi
 XAUTH=$HOME/.Xauthority
 touch $XAUTH
+echo $SOURCE_DIR
 docker run -it --rm \
   -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v $XAUTHORITY:/home/developer/.Xauthority \
-  -v $SOURCE_DIR/:/data \
+  -v "$SOURCE_DIR"/:/data \
   -w /data mullinix/nvidia-opengl-anaconda3-spyder:from-singularity-recipe
 
